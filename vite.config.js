214e75a21bce3e60bc7025cb4e63755112aeb241
Rahofski <roma.rakhov15@gmail.com>
2025-07-02 04:24:25 +0300

    bam...

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom']
+        }
+      }
+    }
+  },
+  server: {
+    open: true
+  }
+});
