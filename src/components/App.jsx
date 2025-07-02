158402e8d0a8889774d1aca2e13c61a034b4f21a
Rahofski <roma.rakhov15@gmail.com>
2025-07-02 04:37:30 +0300

    aa??

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 
 import Header from './Header';
 import Main from './Main';
+import Footer from './Footer';
 
 export default function App() {
   return (
@@ -12,26 +13,3 @@ export default function App() {
   );
 }
 
-function Footer() {
-  return (
-    <footer className="footer">
-      <ul className="footer__list">
-        <li className="footer__item">
-          <a className="footer__link" href="/">Помощь</a>
-        </li>
-        <li className="footer__item">
-          <a className="footer__link" href="/">Обратная связь</a>
-        </li>
-        <li className="footer__item">
-          <a className="footer__link" href="/">Разработчикам</a>
-        </li>
-        <li className="footer__item">
-          <a className="footer__link" href="/">Условия использования</a>
-        </li>
-      </ul>
-      <div className="footer__copyright">
-        © 1997–2023 ООО «Яндекс»
-      </div>
-    </footer>
-  );
-}
