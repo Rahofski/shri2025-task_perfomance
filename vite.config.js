a5cb866c02b9fc746570c88e4756ebf419d1bb2c
Rahofski <roma.rakhov15@gmail.com>
2025-07-02 05:07:44 +0300

    god pls

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/shri2025-task_perfomance',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
-        manualChunks: {
-          react: ['react', 'react-dom']
-        }
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
       }
     }
-  },
-  server: {
-    open: true
   }
-});
+})
